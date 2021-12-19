4916cbe4d3dc98eeb322d7483b2bfb087694cd5a
帽子平头 <41459910+lvlq73@users.noreply.github.com>
2021-12-19 11:10:42 +0800

    服务端demo
    服务端demo

---
+++ service-demo/service-api/src/main/java/com/llq/api/IHelloService.java
@@ -0,0 +1,15 @@
+package com.llq.api;
+
+import com.llq.netty.scan.RpcApi;
+
+/**
+ * @author lvlianqi
+ * @description 测试rpc接口
+ * @createDate 2019/11/27 21:14
+ */
+@RpcApi(alias = "helloService", serviceId = "service_test")
+public interface IHelloService {
+    String hello(String name);
+
+    int sum(int one, int two);
+}
